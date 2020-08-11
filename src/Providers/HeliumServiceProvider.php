a612de3bb0f1de0083265698d54d77f36c8a3633
Simon Morris <simonsos@hotmail.co.uk>
2020-08-11 23:13:14 +0100

    provider

---
+++ src/Providers/HeliumServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Helium\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HeliumServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
